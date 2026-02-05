0d366f2164465fe71ae5c01858352cb89926ad6f
KrisDaneM <krisdane1234@gmail.com>
2026-02-05 18:24:49 +0800

    Add splash screen feature and pubspec.yaml update

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // Temporary logic: Wait 3 seconds then go to Login
-    Future.delayed(const Duration(seconds: 3), () {
-      if (context.mounted) context.go('/login');
-    });
+  State<SplashScreen> createState() => _SplashScreenState();
+}
 
-    return const Scaffold(
-      backgroundColor: Color(0xFF1E2648), // Your Navy Blue
-      body: Center(child: Text("Splash Screen", style: TextStyle(color: Colors.white))),
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) context.go('/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF162651),
+      body: Center(
+        child: Image.asset(
+          'lib/core/assets/images/splash_image.png',
+          fit: BoxFit.contain,
+          width: 250,
+        ),
+      ),
     );
   }
 }
